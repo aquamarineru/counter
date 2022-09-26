1acb52de7d10d668d0c6878b689e9f9f6e2fe5e1
Marina Ilyushina <m_ilyushina@hotmail.com>
2022-09-26 20:20:15 +0200

    counter

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import {useState} from 'react';
 import './App.css';
 
-function App() {
+function App(props) {
+
+  const [counter, setCounter] = useState(props.counter)
+
+  const incCounter = () => {
+    if(counter < 50 ) {
+      setCounter(counter => counter +1)
+    }
+  }
+
+  const decCounter = () => {
+    if(counter > -50 ) {
+      setCounter(counter => counter -1)
+    }
+  }
+  const rndCounter = () => {
+    setCounter(+(Math.random() * (50 - -50) + -50).toFixed(0))
+  }
+  const resetCounter = () => {
+    setCounter(props.counter)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="counter">{counter}</div>
+      <div className="controls">
+        <button onClick={incCounter}>INC</button>
+        <button onClick={decCounter}>DEC</button>
+        <button onClick={rndCounter}>RND</button>
+        <button onClick={resetCounter}>RESET</button>
+      </div>
     </div>
   );
 }
 
 export default App;
+
+
+  // Используйте только стрелочную форму методов
+  // Почему? Подробный ответ будет в следующем уроке
+  ReactDOM.render(
+    <App counter={0}/>,
+  document.getElementById('root')
+  );
+
+
+
+
+// 1) Начальное значение счетчика должно передаваться через props
+// 2) INC и DEC увеличивают и уменьшают счетчик соответственно на 1. Без ограничений, но можете добавить границу в -50/50. По достижению границы ничего не происходит
+// 3) RND изменяет счетчик в случайное значение от -50 до 50. Конструкцию можете прогуглить за 20 секунд :) Не зависит от предыдущего состояния
+// 4) RESET сбрасывает счетчик в 0 или в начальное значение из пропсов. Выберите один из вариантов
